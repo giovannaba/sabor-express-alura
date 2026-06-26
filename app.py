b5e598e789baeb39141eeef2b3d1e508db829797
giovannaba <giovannaba01@gmail.com>
2026-06-26 12:11:38 -0300

    aula 01

---
+++ app.py
@@ -0,0 +1,11 @@
+print("""
+𝕤𝕒𝕓𝕠𝕣 𝕖𝕩𝕡𝕣𝕖𝕤𝕤
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Voce escolheu a opção {opcao_escolhida}')
